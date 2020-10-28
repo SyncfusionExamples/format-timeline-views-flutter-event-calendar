408d9d4b3f1dcb42d8a5fdde1cec4d8d193470a4
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-10-28 18:32:16 +0530

    FLUT-2630 - Sample added

---
+++ flutter_datetimeformat_custom/android/app/src/main/kotlin/com/example/flutter_datetimeformat_custom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_datetimeformat_custom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_datetimeformat_custom/lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(TimelineViewsDateTimeFormat());
+
+class TimelineViewsDateTimeFormat extends StatefulWidget {
+  @override
+  TimelineViewsDateTimeFormatState createState() =>
+      TimelineViewsDateTimeFormatState();
+}
+
+List<String> _views = <String>[
+  'TimelineDay',
+  'TimelineWeek',
+  'TimelienWorkWeek',
+  'TimelineMonth'
+];
+
+class TimelineViewsDateTimeFormatState extends State<TimelineViewsDateTimeFormat> {
+  CalendarController _calendarController;
+  TimeSlotViewSettings timeSlotViewSettings;
+
+  @override
+  void initState() {
+    _calendarController = CalendarController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              leading: PopupMenuButton<String>(
+                icon: Icon(Icons.calendar_today),
+                itemBuilder: (BuildContext context) =>
+                    _views.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList(),
+                onSelected: (String value) {
+                  setState(() {
+                    if (value == 'TimelineDay') {
+                      _calendarController.view = CalendarView.timelineDay;
+                      timeSlotViewSettings=TimeSlotViewSettings(dayFormat: 'EEE', dateFormat: 'dd', timeFormat: 'hh:mm a');
+                    } else if (value == 'TimelineWeek') {
+                      _calendarController.view = CalendarView.timelineWeek;
+                      timeSlotViewSettings=TimeSlotViewSettings(dayFormat: 'EEE', dateFormat: 'dd', timeFormat: 'hh:mm a');
+                    } else if (value == 'TimelimeWorkWeek') {
+                      _calendarController.view = CalendarView.timelineWorkWeek;
+                      timeSlotViewSettings=TimeSlotViewSettings(dayFormat: 'EEE', dateFormat: 'dd', timeFormat: 'hh:mm a');
+                    } else if (value == 'TimelineMonth') {
+                      _calendarController.view = CalendarView.timelineMonth;
+                      timeSlotViewSettings=TimeSlotViewSettings(dayFormat: 'EEEE');
+                    }
+                  });
+                },
+              ),
+            ),
+            body: SafeArea(
+              child: SfCalendar(
+                controller: _calendarController,
+                dataSource: _getDataSource(),
+                monthViewSettings: MonthViewSettings(showAgenda: true),
+                timeSlotViewSettings: timeSlotViewSettings
+              ),
+            )));
+  }
+
+  _DataSource _getDataSource() {
+    final List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(hours: 4)),
+      endTime: DateTime.now().add(Duration(hours: 5)),
+      subject: 'Meeting',
+      color: Colors.red,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 5)),
+      subject: 'Development Meeting   New York, U.S.A',
+      color: Color(0xFFf527318),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      subject: 'Project Plan Meeting   Kuala Lumpur, Malaysia',
+      color: Color(0xFFfb21f66),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      subject: 'Support - Web Meeting   Dubai, UAE',
+      color: Color(0xFFf3282b8),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 1)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      subject: 'Project Release Meeting   Istanbul, Turkey',
+      color: Color(0xFFf2a7886),
+    ));
+    appointments.add(Appointment(
+        startTime: DateTime.now().add(const Duration(hours: 4, days: -1)),
+        endTime: DateTime.now().add(const Duration(hours: 5, days: -1)),
+        subject: 'Release Meeting',
+        color: Colors.lightBlueAccent,
+        isAllDay: true));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 2, days: -4)),
+      endTime: DateTime.now().add(const Duration(hours: 4, days: -4)),
+      subject: 'Performance check',
+      color: Colors.amber,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 11, days: -2)),
+      endTime: DateTime.now().add(const Duration(hours: 12, days: -2)),
+      subject: 'Customer Meeting   Tokyo, Japan',
+      color: Color(0xFFffb8d62),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 6, days: 2)),
+      endTime: DateTime.now().add(const Duration(hours: 7, days: 2)),
+      subject: 'Retrospective',
+      color: Colors.purple,
+    ));
+
+    return _DataSource(appointments);
+  }
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
